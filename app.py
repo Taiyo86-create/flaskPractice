c0f55063db83e32a9a70c278baa746c3493c2ca6
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-07 23:12:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder='templates2')
 def index():
     return render_template('index.html')
 
-@app.route('/home/<string;user_name>')
+@app.route('/home/<string:user_name>')
 def home(user_name):
     login_user = user_name
     return render_template('home.html', login_user=login_user)
