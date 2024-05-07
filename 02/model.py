99e16fa439acfe0386e01c25a6b954da2db0aa74
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-07 23:39:31 +0900

    Update model.py

--- 02/model.py
+++ 02/model.py
@@ -10,3 +10,8 @@ app = Flask(__name__)
 # データベース設定
 database_path = os.path.join(base_dir, 'data.sqlite')
 app.config['SQLALCHEMY_DATABASE_URL'] = f'sqlite:///{database_path}'
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(app, model_class=Base)
