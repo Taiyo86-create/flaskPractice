23a75eb41aa29d3cfbfa4f025877cd75ab58f873
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-07 23:34:32 +0900

    Update model.py

--- 02/model.py
+++ 02/model.py
@@ -1,4 +1,12 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import Mapped, DeclarativeBase
+from sqlalchemy.orm import Mapped, DeclarativeBase, mapped_column
+
+base_dir = os.path.dirname(__file__)
+
+app = Flask(__name__)
+
+# データベース設定
+database_path = os.path.join(base_dir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URL'] = f'sqlite:///{database_path}'
