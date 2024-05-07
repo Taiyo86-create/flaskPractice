b1f82a17d62b8316b7962c54da1a7d1a14fc9cf3
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-07 23:07:47 +0900

    動的サイト

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__, template_folder='templates2')
 def index():
     return render_template('index.html')
 
+@app.route('/home/<string;user_name>')
+def home(user_name):
+    login_user = user_name
+    return render_template('home.html', login_user=login_user)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
