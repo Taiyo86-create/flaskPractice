e4f0ccf71ae9f8b486b74cdbe0f14e1b00c8e575
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-08 00:04:02 +0900

    Update model.py

--- 02/model.py
+++ 02/model.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import Mapped, DeclarativeBase, mapped_column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import mapped_column
 
 base_dir = os.path.dirname(__file__)
 
@@ -9,14 +10,20 @@ app = Flask(__name__)
 
 # データベース設定
 database_path = os.path.join(base_dir, 'data.sqlite')
-app.config['SQLALCHEMY_DATABASE_URL'] = f'sqlite:///{database_path}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{database_path}'
 
-class Base(DeclarativeBase):
-    pass
+Base = declarative_base()
 
 db = SQLAlchemy(app, model_class=Base)
 
 class Person(db.Model):
     __tablename__ = 'person'
-    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(db.String)
+    id = mapped_column(db.Integer, primary_key=True)
+    name = mapped_column(db.String)
+    age = mapped_column(db.Integer)
+    
+    def __str__(self):
+        return f"id={self.id}, name={self.name}, age={self.age}"
+    
+print(db)
+print(db.metadata.tables)
